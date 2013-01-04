df282631f30bf781410149dc2a9ae148a46b985c
Aaron Druck <aldruck@gmail.com>
2013-01-04 15:49:50 -0800

    new commit

--- index.php
+++ index.php
@@ -101,35 +101,16 @@
 </head> 
 <body onload="init()"> 
 
-<div data-role="page" id="page1">
-
-	<div data-role="header">
-		<h1>Create an app</h1>
-	</div><!-- /header -->
-
-	<div data-role="content">	
-		<ul data-role="listview" data-inset="true" data-filter="false">
-			<li><a href="#page2" data-transition="slide">Slingshot</a></li>
-		</ul>		
-	</div><!-- /content -->
-	
-	<div data-role="footer">
-		<h4>Page Footer</h4>
-	</div><!-- /footer -->
-	
-</div><!-- /page -->
-
 <!-- Start of INSTRUMENT IDEA -->
 <div data-role="page" id="page2">
 
 	<div data-role="header">
-		<a href="#" data-rel="back" data-icon="arrow-l">Back</a>
 		<h1>Draw a picture</h1>
-		<a href="slingshot.php" data-transition="slide" data-icon="arrow-r"  rel="external" onclick="saveViaAJAX();">Next</a>
+		<a href="#page4" data-transition="slide" data-icon="arrow-r"  rel="external" onclick="saveViaAJAX();">Next</a>
 	</div><!-- /header -->
 
 	<div data-role="content">	
-		<p>Draw your instrument:</p>	
+		<p>Draw a picture:</p>	
 		
 
 	  	<canvas id="paintBox">
@@ -141,9 +122,7 @@
 		<script type="text/javascript">
 		function saveViaAJAX()
 		{
-			
-
-			
+				
 			var testCanvas = document.getElementById("paintBox");
 			var canvasData = testCanvas.toDataURL("image/png");
 			var postData = "canvasData="+canvasData;
@@ -163,21 +142,19 @@
 		  	}
 
 			ajax.send(postData);
-			alert(canvasData);
+			//alert(canvasData);
 			
 			//UPDATE PROCESSING IMAGE VARIABLE HERE OR FIRE OFF PROCESSING SKETCH SO IT RENDERS
-			
+			var pjs = Processing.getInstanceById('mycanvas');
+			//alert(canvasData);
+			var text = canvasData;
+		    pjs.newImage(text);
 			///
 			
 		}
 
 		</script>
-		
-		<!-- <div id="canvas-area">
-			<canvas id="can" width="400"height="400" style="position:relative;top:10%;left:10%;border:2px solid;"></canvas>
-	    	<input type="button" value="clear" id="clr" size="23" onclick="erase()"style="position:absolute;top:55%;left:15%;">
-		</div> -->
-		
+				
 			
 	</div><!-- /content -->
 
@@ -192,7 +169,6 @@
 	<div data-role="header">
 		<a href="#" data-rel="back" data-icon="arrow-l">Back</a>
 		<h1>Preview your instrument:</h1>
-		<a href="#page1" data-transition="pop">Done</a>
 	</div><!-- /header -->
 
 	<div data-role="content">	
@@ -263,9 +239,6 @@
 				<div id="p1"></div>
 				<div id="instrument-button">	
 					<!-- <img id="canvasimg" style="display:none;"> -->
-					<script type="text/javascript" charset="utf-8">
-						// alert(document.getElementById("canvasimg").src);
-					</script>
 				</div>
 				
 				<canvas id="mycanvas"></canvas>
@@ -281,13 +254,13 @@
 				
 				<script type="text/processing" data-processing-target="mycanvas">
 					//initialize variables
-					int leftRodTopX = 140;
-					int leftRodTopY = 360;
-					int rightRodTopX = 165;
-					int rightRodTopY = 360;
+					int leftRodTopX = 120;
+					int leftRodTopY = 60;
+					int rightRodTopX = 185;
+					int rightRodTopY = 60;
 					int rectLeftX = 150;
 					int rectMidX = 155;
-					int rectTopY = 400;
+					int rectTopY = 200;
 					int rectWidth = 10;
 					int rectHeight = 100;
 					float slingMid = 152.5;
@@ -306,66 +279,37 @@
 					PVector release;
 
 					float gravity = 0;
-					
-					// PImage b;
-					// String uri = jsString;
-					// im = loadImage(uri);
-					// String s = uri;
-				    // PImage img;
-				
+
 					PImage b;
-					b = loadImage("images/test.png");
-					//preloading the image here before it's updated
 
-
-									
 					void setup(){
 					
 						size(290,400);
 						background(255);
 						smooth();
 						fill(50);
-						//text(s, 10, 10, 70, 280);
-						
-						// make this dynamic this from the Javascript to update here, otherwise just grabs pre-existing one:
-						//img = loadImage(uri);
-						
-						//String url = "http://mt0.google.com/mt?n=404&v=w2.61&x=9913&y=12119&zoom=2";
-
-						//online = loadImage(url, "png");
-
-						//noLoop();
-						
-						/////////
-						
-						// http://processingjs.org/articles/p5QuickStart.html
-						// http://processingjs.nihongoresources.com/interfacing/
-						// http://processingjs.org/articles/PomaxGuide.html
-						// http://forum.processing.org/topic/how-to-load-an-image-in-base64-with-processing-js
-						// https://wiki.mozilla.org/Processing.js_for_JavaScript_Devs
-						
-												
+																		
 						//create initial slingshot
 						line(leftRodTopX,leftRodTopY,rightRodTopX,rightRodTopY); //string
+						newImage();
 						
-						ellipse(150, 150, 40, 40);
-						fill(255,0,0);
-						
+					}
+					
+					void newImage(String imageFile) {
+						//println("test");
+						b = loadImage(imageFile);
 					}
 
 					void draw(){
 						
-						
-						image(b, 0, 0);
+						image(b, 50, 40, 300, 416);
 						
 						if(mousePressed){
-						  // background(255);
+						  background(255);
 						  bezier(900,400,880,375,880,325,912,320);
 						  fill(255,0,0);
 						  
-						  //image(img, 150, mouseY, 100, 100);
-						  
-						  ellipse(150, mouseY, 40, 40);
+						  image(b, 50, mouseY-240, 300, 416);
 						  noFill();
 						  bezier(rightRodTopX,rightRodTopY,150,mouseY,150,mouseY,leftRodTopX,leftRodTopY);
 						  fill(0,0,255);
@@ -376,7 +320,7 @@
 						}
 
 						if(mousePressed==false){
-						  //background(255);
+						  background(255);
 						  noFill();
 						  line(rectMidX,rectTopY,rightRodTopX,rightRodTopY);       //right rod
 						  line(rectMidX,rectTopY,leftRodTopX,leftRodTopY);         //left rod
@@ -389,28 +333,25 @@
 						   noFill();
 						   bezier(900,400,880,375,880,325,912,320);
 						   fill(255,0,0);
-						   ellipse(150, release.y, 40, 40);
+						   image(b, 50, release.y-240, 300, 416);
+						   //ellipse(150, release.y, 40, 40);
 						   release.add(press);
 						   release.y+=gravity;
 						   //gravity+=0.6;
 						   //println(release.y); 
 						   noFill();
 						   bezier(925,400,945,375,945,325,912,320);
+						   
+						   
+						   
 
-						   if(release.y>=330&&release.y<=380&&release.x>=900&&release.x<=1000){
+						   if(release.y<=0){
 						     winner = true;
 						   }
 						 }
 
 						 if(winner){
-						   x = random(0,1200);
-						   y = random(0,500);
-						   r = random(0,255);
-						   g = random(0,255);;
-						   b = random(0,255);
-						   fill(r,g,b);
-						   ellipse(x,y,50,50);
-						   println(winner);
+						   //alert("test");
 						 }
 					}
 
